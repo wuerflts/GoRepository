641adcdec834b25a0d8eff8ea503cd335baa1cd3
wuerflts <wuerflts@outlook.de>
2014-03-16 00:57:51 +0100

    Finished google Examples
    added to other functions and a test;
    imported the example code from google;
    moved every file to github.com/user (why user?);

---
+++ user/hello/hello.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+		"fmt"
+
+		"github.com/user/newmath"
+)
+
+func main() {
+		fmt.Printf("Hello, world. Sqrt(2) = %v\n", newmath.Sqrt(2))
+		fmt.Printf("Hello, world. Sqr (2) = %v\n", newmath.Sqr(2))
+}
---
+++ user/newmath/sqr.go
@@ -0,0 +1,7 @@
+package newmath
+
+// Sqr returns the square of x.
+func Sqr(x float64)  float64 {
+		z := x * x
+		return z
+}
